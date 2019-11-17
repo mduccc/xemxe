74fc052ca30b901aa6b91983131b25f293c373fa
mduccc <duc2521997@gmail.com>
2019-11-17 23:43:58 +0700

    Add Label.kt

---
+++ app/src/main/java/com/indieteam/binh_lieu_app/views/Label.kt
@@ -0,0 +1,14 @@
+package com.indieteam.binh_lieu_app.views
+
+class Label {
+    companion object{
+        const val tuyenxe = "Tuyến xe: "
+        const val giave = "Giá vé: "
+        const val thoigian = "Thời gian: "
+        const val phone = "Điện thoại: "
+        const val soghe = "Số ghế: "
+        const val bienso = "Biển số: "
+        const val vnd = "VNĐ"
+        const val ghichu = "Ghi chú: "
+    }
+}
