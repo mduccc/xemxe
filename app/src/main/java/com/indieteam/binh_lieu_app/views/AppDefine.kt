82f3db36bb4a2d1dcf92346e68fa8300bb23112f
mduccc <duc2521997@gmail.com>
2019-11-17 23:43:36 +0700

    Add AppDefine.kt

---
+++ app/src/main/java/com/indieteam/binh_lieu_app/views/AppDefine.kt
@@ -0,0 +1,10 @@
+package com.indieteam.binh_lieu_app.views
+
+import android.Manifest
+
+class AppDefine {
+    companion object {
+        const val requestCode = 0
+        val permissions = arrayOf(Manifest.permission.CALL_PHONE)
+    }
+}
