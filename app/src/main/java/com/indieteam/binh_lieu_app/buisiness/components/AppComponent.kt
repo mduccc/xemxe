0abddce91ab0f9e07576fbfd2c5d5241b50bc811
mduccc <duc2521997@gmail.com>
2019-11-17 23:46:54 +0700

    Modify APpComponent.kt

--- app/src/main/java/com/indieteam/binh_lieu_app/buisiness/components/AppComponent.kt
+++ app/src/main/java/com/indieteam/binh_lieu_app/buisiness/components/AppComponent.kt
@@ -3,6 +3,7 @@ package com.indieteam.binh_lieu_app.buisiness.components
 import com.indieteam.binh_lieu_app.buisiness.modules.*
 import com.indieteam.binh_lieu_app.buisiness.providers.CoachProvider
 import com.indieteam.binh_lieu_app.buisiness.providers.TaxiProvider
+import com.indieteam.binh_lieu_app.views.DetailActivity
 import com.indieteam.binh_lieu_app.views.MainActivity
 import com.indieteam.binh_lieu_app.views.RecyclerAdapter
 import com.indieteam.binh_lieu_app.views.TabFragment
@@ -16,4 +17,5 @@ interface AppComponent {
     fun inject(mainActivity: MainActivity)
     fun inject(recyclerAdapter: RecyclerAdapter)
     fun inject(tabFragment: TabFragment)
+    fun inject(detailActivity: DetailActivity)
 }
